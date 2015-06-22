2751a3d37d9e76d1b4681fccf4e0a5a165019b79
Brandon Stansbury <brandonrstansbury@gmail.com>
2015-06-21 23:40:38 -0700

    add test class in library to be called from console app

---
+++ CppTestArenaLib/LibThing.cpp
@@ -0,0 +1,18 @@
+//
+//  LibThing.cpp
+//  CppTestArenaLib
+//
+//  Created by Brandon Stansbury on 6/21/15.
+//  Copyright (c) 2015 Brandon Stansbury. All rights reserved.
+//
+
+#include <iostream>
+#include "LibThing.h"
+
+namespace cpptestarena {
+    
+    void LibThing::printStuff() const
+    {
+        std::cout << "LibThing contains " << this->v << std::endl;
+    }
+}
---
+++ CppTestArenaLib/LibThing.h
@@ -0,0 +1,28 @@
+//
+//  LibThing.h
+//  CppTestArenaLib
+//
+//  Created by Brandon Stansbury on 6/21/15.
+//  Copyright (c) 2015 Brandon Stansbury. All rights reserved.
+//
+
+#ifndef CppTestArenaLib_LibThing_h
+#define CppTestArenaLib_LibThing_h
+
+namespace cpptestarena {
+    
+    class LibThing {
+    public:
+        explicit LibThing(int value)
+        :v{value}
+        {
+        }
+        
+        void printStuff() const;
+        
+    private:
+        int v;
+    };
+}
+
+#endif
